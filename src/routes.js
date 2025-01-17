e847e32211931138ed5d62eca4f52609137fca8e
Christopher Giroir <kelsin@valefor.com>
2025-01-16 23:56:34 -0800

    feat: add simple non-saved random response

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { map } from "ramda";
+
 import pkg from "#pkg" with { type: "json" };
 import * as db from "#src/db/index.js";
 import { game } from "#src/messages/game.js";
 import { player } from "#src/messages/player.js";
 import * as server from "#src/messages/server.js";
+import { random } from "#src/random.js";
 import validate from "#src/validate.js";
 
 const get_base_url = (req) => {
@@ -41,6 +46,16 @@ export const post_player = async (req, res) => {
   // const base_url = get_base_url(req);
   // const game_id = req.params.game_id;
   // const player_id = req.params.player_id;
+  const message = req.body;
+  switch (message.type) {
+    case "client.random":
+      return res.json({
+        type: "server.random",
+        id: uuidv4(),
+        request: message.request,
+        result: map(random, message.request),
+      });
+  }
 
   return res.json({});
 };
